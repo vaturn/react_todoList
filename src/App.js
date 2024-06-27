8ec63b783c491052eaf224463c98408a273df703
vaturn <will6867@naver.com>
2024-06-27 20:32:02 +0900

    [3/n] Add TodoItemList react component to display todo items: Make App component to have todo items as its state

--- src/App.js
+++ src/App.js
@@ -35,10 +35,12 @@ const TodoItemList = (props) => {
 };
 
 function App() {
+  const [todoItemList, setTodoItemList] = useState([]);
+
   return (
     <div className="App">
       <TodoItemInputField onSubmit={() => {}} />
-        <TodoItemList todoItemList={[]} />
+        <TodoItemList todoItemList={todoItemList} />
     </div>
   );
 }

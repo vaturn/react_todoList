cdfee7e39510dbad632721c8569fb05fd3a210e0
vaturn <will6867@naver.com>
2024-06-30 19:38:22 +0900

    Add remove button on TodoItem component

--- src/App.js
+++ src/App.js
@@ -33,13 +33,19 @@ const TodoItem = (props) => {
       <span style={style} onClick={() => 
         props.onTodoItemClick(props.todoItem)
       }>{props.todoItem.todoItemContent}</span>
+      <Button variant='outlined' onClick={() => props.onRemoveClick(props.todoItem)}>Remove</Button>
     </li>
   );
 };
 
 const TodoItemList = (props) => {
   const todoList = props.todoItemList.map((todoItem, index) =>{
-    return <TodoItem key={index} todoItem={todoItem} onTodoItemClick={props.onTodoItemClick}/>;
+    return <TodoItem 
+      key={index} 
+      todoItem={todoItem} 
+      onTodoItemClick={props.onTodoItemClick}
+      onRemoveClick={props.onRemoveClick}
+    />;
   });
 
   return (<div>
@@ -73,12 +79,20 @@ function App() {
     }));
   };
 
+  const onRemoveClick = (removedTodoItem) => {
+    console.log(removedTodoItem.id);
+    setTodoItemList(todoItemList.filter((todoItem) => {
+      return todoItem.id !== removedTodoItem.id;
+    }));
+  }
+
   return (
     <div className="App">
       <TodoItemInputField onSubmit={onSubmit} />
         <TodoItemList 
           todoItemList={todoItemList}
           onTodoItemClick={onTodoItemClick}
+          onRemoveClick={onRemoveClick}
         />
     </div>
   );
